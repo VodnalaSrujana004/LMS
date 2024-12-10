876cd5939edac5ba9be59d4a8dda8ca1019c845c
Vodnala Srujana <146533051+VodnalaSrujana004@users.noreply.github.com>
2024-12-10 22:58:54 +0530

    upto brfore middleware

---
+++ auth-tutorial/app/auth/register/page.tsx
@@ -0,0 +1,11 @@
+import { RegisterForm } from "@/components/auth/register-form.tsx";
+
+
+
+const RegisterPage = () => {
+    return ( 
+        <RegisterForm/>
+     );
+}
+ 
+export default RegisterPage;
